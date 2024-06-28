c96ac0d1654d17f7e153adc7c4ef344a93f30cb5
Fabricio Luiz Rodrigues <fabricioflr@gmail.com>
2024-06-27 21:06:34 -0300

    regressivo até o evento

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
-console.log("Olá mundo");
+AOS.init();
+
+const dataDoEvento = new Date("Dec 14, 2024 10:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+const agora = new Date();
+const timeStampAtual = agora.getTime();
+
+const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+const diasAteOEvento = Math.floor(distanciaAteOEvento / (1000 * 60 * 60 * 24));
+const horasAteOEvento = Math.floor((distanciaAteOEvento % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+const minutosAteOEvento = Math.floor((distanciaAteOEvento % (1000 * 60 * 60)) / (1000 * 60));
+const segundosAteOEvento = Math.floor((distanciaAteOEvento % (1000 * 60)) / 1000);
+
+console.log(diasAteOEvento);
+console.log(horasAteOEvento);
+console.log(minutosAteOEvento);
+console.log(segundosAteOEvento);
+
+document.getElementById("contador").innerHTML =`${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+}, 1000);
