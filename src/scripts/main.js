6e2b65dbe2e777bf152df35aaa73dc56e0fa8d21
Fabricio Luiz Rodrigues <fabricioflr@gmail.com>
2024-06-28 02:15:56 -0300

    construindo rodapé

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,15 +9,20 @@ const timeStampAtual = agora.getTime();
 
 const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
 
-const diasAteOEvento = Math.floor(distanciaAteOEvento / (1000 * 60 * 60 * 24));
-const horasAteOEvento = Math.floor((distanciaAteOEvento % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-const minutosAteOEvento = Math.floor((distanciaAteOEvento % (1000 * 60 * 60)) / (1000 * 60));
-const segundosAteOEvento = Math.floor((distanciaAteOEvento % (1000 * 60)) / 1000);
+const dayForMs = 1000 * 60 * 60 * 24;
+const hourForMs = 1000 * 60 * 60;
+const minuteForMs = 1000 * 60;
 
-console.log(diasAteOEvento);
+const diasAteOEvento = Math.floor(distanciaAteOEvento / dayForMs);
+const horasAteOEvento = Math.floor(distanciaAteOEvento % dayForMs / hourForMs);
+const minutosAteOEvento = Math.floor(distanciaAteOEvento % hourForMs / minuteForMs);
+const segundosAteOEvento = Math.floor(distanciaAteOEvento % minuteForMs / 1000);
+
+/*console.log(diasAteOEvento);
 console.log(horasAteOEvento);
 console.log(minutosAteOEvento);
 console.log(segundosAteOEvento);
+*/
 
 document.getElementById("contador").innerHTML =`${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
 
